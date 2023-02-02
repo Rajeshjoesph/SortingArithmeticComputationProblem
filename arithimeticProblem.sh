e9eeb844970d53e4a20aedca81796636a4712ed5
“rajeshjoshph” <rajeshkannan1999.rk@gmail.com>
2023-02-02 14:08:35 -0800

    Add: Program To Take Three Digit Inputs

---
+++ arithimeticProblem.sh
@@ -0,0 +1,8 @@
+read -p "Enter the First Value:" a
+read -p "Enter The Secound Value:" b
+read -p "Enter The Third Value" c
+echo "Value of a is $a"
+echo "Value of b is $b"
+echo "Value of c is $c"
+
+
