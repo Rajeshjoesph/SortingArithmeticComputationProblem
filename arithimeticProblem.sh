55fd67966ec0476b2d8c9d908a710c8668ac90c2
“rajeshjoshph” <rajeshkannan1999.rk@gmail.com>
2023-02-02 15:08:29 -0800

    Add: Compution Problem

--- arithimeticProblem.sh
+++
@@ -1,8 +0,0 @@
-read -p "Enter the First Value:" a
-read -p "Enter The Secound Value:" b
-read -p "Enter The Third Value" c
-echo "Value of a is $a"
-echo "Value of b is $b"
-echo "Value of c is $c"
-
-
---
+++ computeAdditionalVal.sh
@@ -0,0 +1,16 @@
+echo "Welcome To the Sorting Arithmetic Problem"
+printf "\n"
+read -p "Enter The First Value: " a
+read -p "Enter The Secound Value: " b
+read -p "Enter The Thired Value: " c
+printf "\n"
+echo "Value Of a is $a"
+echo "Value Of b is $b"
+echo "Value Of c is $c"
+printf "\n"
+compute=$(( $a + $b * $c ))
+
+echo "Calculation of a+b*c is $compute"
+
+
+
