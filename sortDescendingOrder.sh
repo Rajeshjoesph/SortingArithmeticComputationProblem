9c1162cdf84bb6ec1c7a38ed727e76cd7fc44b48
“rajeshjoshph” <rajeshkannan1999.rk@gmail.com>
2023-02-02 17:16:03 -0800

    Add: Sort Result In Descending Order

--- sortDescendingOrder.sh
+++ sortDescendingOrder.sh
@@ -1,28 +1,30 @@
+printf "\n"
 echo "Welcome To the Sorting Arithmetic Problem"
 printf "\n"
 read -p "Enter The First Value: " a
 read -p "Enter The Secound Value: " b
 read -p "Enter The Thired Value: " c
-printf "\n"
+echo "----------------------------------------------"
 echo "Value Of a is $a"
 echo "Value Of b is $b"
 echo "Value Of c is $c"
-printf "\n"
+echo "----------------------------------------------"
 compute1=$(( $a + $b * $c ))
 compute2=$(( $a * $b + $c ))
 compute3=$(( $c + $b / $b ))
 compute4=$(( $a % $b + $c ))
-
 declare -A result
 result[1]="$compute1"
 result[2]="$compute2"
 result[3]="$compute3"
 result[4]="$compute4"
-
-echo "Store In Dictionary: "${result[@]}
-
 counter=${result[*]}
 array=($counter)
-printf "\n"
-echo "Value from Dictionary Into Array: " ${array[*]}
-
+data=${array[*]}
+echo  $data
+echo "-----------------------------------------------"
+echo "Array in Descending Order: "
+for i in $(echo "$data" | tr ' ' '\n' | sort -nr)
+do
+	echo $i
+done
